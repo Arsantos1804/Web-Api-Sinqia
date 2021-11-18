bdd62aba1410c83ebfa2fa5f0b94f0a3f33ede5e
ARSantos <66450170+Arsantos1804@users.noreply.github.com>
2021-11-18 10:14:39 -0300

    Web API Dotnet

---
+++ Models/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿
+    using Microsoft.EntityFrameworkCore;
+namespace Web_Api_Sinqia.Models
+{
+    
+        public class AppDbContext : DbContext
+        {
+            public AppDbContext(DbContextOptions<AppDbContext> options)
+                : base(options)
+            {
+            }
+
+            public DbSet<Musica> TodaMusicas { get; set; }
+        }
+    }
+
---
+++ Models/Musica.cs
@@ -0,0 +1,11 @@
+﻿namespace Web_Api_Sinqia.Models
+{
+    public class Musica
+    {
+        public int Id { get; set; }
+        public string NomeMusica { get; set; }
+        public string NomeArtista { get; set; }
+        public string Album { get; set; }
+        public string Type { get; set; }
+    }
+}
